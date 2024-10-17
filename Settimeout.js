94ce9a8a06f9805282afccdee7543fb75038eb04
ANKIT <Ankitnayal07@gmail.com>
2024-10-17 11:36:39 +0530

    update

---
+++ Settimeout.js
@@ -0,0 +1,50 @@
+// setTimeout(()=>{
+//     setInterval(()=>{
+//     console.log("Ankit")
+//     },1000)
+// },2000);
+
+// let hr=0;
+// let min=0;
+// let sec=0;
+// setInterval(()=>{
+//     sec++;
+// if(sec==60){
+//     min++;
+//     sec=0;
+// }if(min==60){
+//     hr++;
+//     min=0;
+
+// }
+// console.log("sec",sec);
+// console.log("min",min);
+// console.log("hour",hour);
+// },1000)
+// setTimeout(()=>{
+//  setInterval(()=>{
+//     console.log("Ankit")
+//  },1000
+// )   
+// },2000
+// )
+
+// let hr=0;
+// let  min=0;
+// let sec=0;
+// setInterval(()=>{
+//     sec++;
+// if(sec==60){
+//     min++;
+//     sec=0;
+// }
+// if(min==60){
+//     hour++;
+//     min=0;
+// }
+//     console.log("sec",sec);
+//     console.log("min",min);
+//     console.log("hour",hour);
+
+// },1000
+// )
